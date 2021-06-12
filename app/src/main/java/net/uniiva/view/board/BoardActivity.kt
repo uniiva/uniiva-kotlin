9879ddb9c1153f661046084b8d12b5127059315c
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-06-12 20:04:17 +0900

    [add] createフラグメントを追加

--- app/src/main/java/net/uniiva/view/board/BoardActivity.kt
+++ app/src/main/java/net/uniiva/view/board/BoardActivity.kt
@@ -2,6 +2,9 @@ package net.uniiva.view.board
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import net.uniiva.R
 
 class BoardActivity : AppCompatActivity() {
---
+++ app/src/main/java/net/uniiva/view/board/fragment/CreateFragment.kt
@@ -0,0 +1,23 @@
+package net.uniiva.view.board.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import net.uniiva.databinding.FragmentBoardCreateBinding
+
+class CreateFragment : Fragment(){
+
+    private var _binding: FragmentBoardCreateBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentBoardCreateBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
